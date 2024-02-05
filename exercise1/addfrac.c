d24688ba6705608340c083aecb297532a69a72a8
Zesty <adi8116315284@gmail.com>
2024-02-05 16:00:24 +0530

    adding addfrac.c

---
+++ exercise1/addfrac.c
@@ -0,0 +1,25 @@
+//
+// Created by adi81 on 2/5/2024.
+//
+
+#include <conio.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int num1, denom1, num2, denom2, result_num, result_denom;
+
+    printf("Enter first fraction: ");
+    scanf("%d/%d", &num1, &denom1);
+
+    printf("Enter second fraction: ");
+    scanf("%d/%d", &num2, &denom2);
+
+    result_num = num1 * denom2 + num2 * denom1;
+
+    result_denom = denom1 * denom2;
+
+    printf("The sum is %d/%d\n", result_num, result_denom);
+
+    getch();
+}
