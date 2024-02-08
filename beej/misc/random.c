fbf259ca42cf3eee4df60ce99b6c83e892a5929f
Zesty <adi8116315284@gmail.com>
2024-02-08 22:50:47 +0530

    adding ./beej/misc/random.c

---
+++ beej/misc/random.c
@@ -0,0 +1,20 @@
+//
+// Created by adi81 on 2/8/2024.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int r;
+
+    do {
+        r = rand() % 100;
+        printf("%d\n", r);
+    } while (r != 37);      // Repeat until r becomes 37
+
+
+    getchar();
+    return 0;
+}
