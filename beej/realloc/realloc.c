9c22672444a6b993df54b0fc91bf69fd06d43fa2
Zesty <adi8116315284@gmail.com>
2024-02-17 22:30:20 +0530

    adding and modifying files such as realloc.c

---
+++ beej/realloc/realloc.c
@@ -0,0 +1,68 @@
+//
+// Created by adi81 on 2/17/2024.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char *readline(FILE *fp);
+
+int main(void) {
+    FILE *fp = fopen("foo.txt", "r");
+
+    char *line;
+
+    while ((line = readline(fp)) != NULL) {
+        printf("%s\n", line);
+        free(line);
+    }
+
+    fclose(fp);
+}
+
+char *readline(FILE *fp) {
+    int offset = 0;
+    int bufsize = 4;
+    char *buf;
+    int c;
+
+    buf = malloc(bufsize);
+
+    if (buf == NULL) {
+        return NULL;
+    }
+
+    while (c = fgetc(fp), c != '\n' && c != EOF) {
+        if (offset == bufsize - 1) {
+            bufsize *= 2;
+
+            char *new_buf = realloc(buf, bufsize);
+
+            if (new_buf == NULL) {
+                free(buf);
+                return NULL;
+            }
+
+            buf = new_buf;
+        }
+
+        buf[offset++] = c;
+    }
+
+    if (c == EOF && offset == 0) {
+        free(buf);
+        return NULL;
+    }
+
+    if (offset < bufsize - 1) {
+        char *new_buf = realloc(buf, offset + 1);
+
+        if (new_buf != NULL) {
+            buf = new_buf;
+        }
+    }
+
+    buf[offset] = '\0';
+
+    return buf;
+}
